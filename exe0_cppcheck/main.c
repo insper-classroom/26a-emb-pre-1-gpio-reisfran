2ce26e699d481f4f25ab0111ff75d1d73fb38ab5
Francisco <franciscomefr@al.insper.edu.br>
2026-02-20 09:44:39 -0300

    commit1

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -5,14 +5,15 @@
 
 int multiplyNumbers(int x, int y) {
     int product = x * y;
+    return product;
 }
 
 int main(void) {
-    int a, b = 5; //uninitvar, legacyUninitvar
+    int a= 2, b = 5; //uninitvar, legacyUninitvar
     int result = multiplyNumbers(a, b);
 
     int arr[NUM_ELEMENTS];
-    for(int i = 0; i <= NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
+    for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
         arr[i] = i;
     }
 
