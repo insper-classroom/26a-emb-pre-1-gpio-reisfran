ce372bda2160c390731eff0421318c4fa27094eb
Francisco <franciscomefr@al.insper.edu.br>
2026-02-21 16:58:20 -0300

    exs

--- exe4/main.c
+++ exe4/main.c
@@ -2,10 +2,47 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int BTN_PIN = 28;
+const int LED_PIN_R = 5;
+const int LED_PIN_P = 8;
+const int LED_PIN_B = 11;
+const int LED_PIN_G = 15;
+
 int main() {
   stdio_init_all();
 
+  gpio_init(BTN_PIN);
+  gpio_set_dir(BTN_PIN, GPIO_IN);
+  gpio_pull_up(BTN_PIN);
+  gpio_init(LED_PIN_R);
+  gpio_set_dir(LED_PIN_R, GPIO_OUT);
+  gpio_init(LED_PIN_P);
+  gpio_set_dir(LED_PIN_P, GPIO_OUT);
+  gpio_init(LED_PIN_B);
+  gpio_set_dir(LED_PIN_B, GPIO_OUT);
+  gpio_init(LED_PIN_G);
+  gpio_set_dir(LED_PIN_G, GPIO_OUT);  
+
   while (true) {
-    // Use delay de 300 ms entre os estados!
+    if(gpio_get(BTN_PIN) == 0) {
+      gpio_put(LED_PIN_R, 1);
+      sleep_ms(300);
+      gpio_put(LED_PIN_R, 0);
+
+      gpio_put(LED_PIN_P, 1);
+      sleep_ms(300);
+      gpio_put(LED_PIN_P, 0);
+
+      gpio_put(LED_PIN_B, 1);
+      sleep_ms(300);
+      gpio_put(LED_PIN_B, 0);
+
+      gpio_put(LED_PIN_G, 1);
+      sleep_ms(300);
+      gpio_put(LED_PIN_G, 0);
+
+      while(gpio_get(BTN_PIN) == 0) {
+      }
+    } 
   }
 }
--- exe5/main.c
+++ exe5/main.c
@@ -21,11 +21,21 @@ int main() {
     while (true) {
 
         if (!gpio_get(BTN_PIN)) {
-            printf("Botao 1: %d\n", cnt_1++);
-        }
+            sleep_ms(20);
+            if(!gpio_get(BTN_PIN)) {
+                printf("Botao 1: %d\n", cnt_1++);
+                while(gpio_get(BTN_PIN) == 0) {
+            }
+        }}
 
         if (!gpio_get(BTN_PIN_2)) {
-            printf("Botao 2: %d\n", cnt_2++);
+            sleep_ms(20);
+            if(!gpio_get(BTN_PIN_2)) {
+                printf("Botao 2: %d\n", cnt_2++);
+                while(gpio_get(BTN_PIN) == 0) {
+            }
         }
     }
 }
+}
+
